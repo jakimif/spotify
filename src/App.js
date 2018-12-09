6460841bccb0febd2615f1b8e9d00e4907180344
André França <andrefranca@andrefranca.local>
2018-12-09 09:50:26 -0200

    Configuring Redux & Saga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
@@ -11,24 +12,27 @@ import Header from './components/Header';
 import { Wrapper, Container, Content } from './styles/components';
 
 import Routes from './routes';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
       <>
         <GlobalStyle />
-        <BrowserRouter>
-          <Wrapper>
-            <Container>
-              <Sidebar />
-              <Content>
-                <Header />
-                <Routes />
-              </Content>
-            </Container>
-            <Player />
-          </Wrapper>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Wrapper>
+              <Container>
+                <Sidebar />
+                <Content>
+                  <Header />
+                  <Routes />
+                </Content>
+              </Container>
+              <Player />
+            </Wrapper>
+          </BrowserRouter>
+        </Provider>
       </>
     );
   }
